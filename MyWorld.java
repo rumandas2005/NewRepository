1d83dc67dd692022f12d1ebc6e6d42721c009858
Ruman Das <ruman.das@lvjusd.k12.ca.us>
2021-11-03 13:25:40 -0700

    new file

---
+++ MyWorld.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class MyWorld extends World
+{
+
+    
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        addObject(new Robot(4, 400, "Sam"), 100, 100);
+    }
+}
---
+++ Robot.java
@@ -0,0 +1,61 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Robot extends Actor
+{
+   int speed;
+   int battery;
+   String name;
+   
+   public Robot(int speed, int battery, String name) {
+        
+        this.speed=speed;
+        this.battery=battery;
+        this.name=name;
+       
+        
+     }
+
+      public void act() 
+    {
+     int counter = 0;
+    
+     move(this.speed);
+     
+    
+        if(getX() == 599 && getY() == 100) {
+            
+            turn(90);
+            move(this.speed);
+        } else if (getX() == 599 && getY() == 399) {
+            turn(90);
+            move(this.speed);
+            
+        } else if(getX() == 0 && getY() == 399) {
+            
+            turn(90);
+            move(this.speed);
+        } else if(getX() == 0 && getY() == 0) {
+            
+            turn(90);
+            move(this.speed);
+        }
+       
+     if(counter % 3 == 0 && this.battery > 0) {
+         
+         this.battery--;
+      } else {
+         this.speed = 0;
+      }
+
+     
+        getWorld().showText(this.toString(), 300, 200);
+        counter++;
+   }
+     public String toString() {
+       
+     return "Name: " +this.name+ " \n Speed: " +this.speed+ " \n Battery: " +this.battery ; 
+     }
+     
+}
+
